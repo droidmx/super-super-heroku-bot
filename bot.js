148b87cdf5939773183ef53aad92ad227ddbb092
Sreyaash <33704504+droidmx@users.noreply.github.com>
2018-05-01 23:58:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
 
-
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerCommandsIn(__dirname + "/commands");
 
 bot.on('ready', () => {
     console.log('I am ready!');
